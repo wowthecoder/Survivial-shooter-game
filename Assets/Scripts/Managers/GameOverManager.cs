7c585ab55bb8380f8c696a62e7214e2b09b485f8
Andrew Loo <82577844+wowthecoder@users.noreply.github.com>
2022-09-20 17:49:48 +0800

    Add files via upload

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Button restartbutton;
+
+    Animator anim;
+    bool showedButton;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            if (!showedButton)
+            {
+                showedButton = true;
+                StartCoroutine(ShowButton());
+            }
+        }
+        else
+        {
+            if (restartbutton.gameObject.activeSelf) 
+            {
+                showedButton = false;
+                restartbutton.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator ShowButton() 
+    {
+        yield return new WaitForSeconds(4);
+        restartbutton.gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Hello");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
